18fb6d3b45c05c046b16847d1f461324b914046e
Ash Monsh <wh7r.com@gmail.com>
2024-07-17 16:13:17 +0300

    fix if the field is from a preset

--- src/Fields/FieldsContract.php
+++ src/Fields/FieldsContract.php
@@ -90,7 +90,7 @@ abstract class FieldsContract implements Arrayable, Fields
                     return null;
                 }
 
-                if ($zeusField->section->form->extensions !== 'LaraZeus\\BoltPro\\Extensions\\Grades') {
+                if (!$zeusField instanceof FieldPreset && $zeusField->section->form->extensions !== 'LaraZeus\\BoltPro\\Extensions\\Grades') {
                     return null;
                 }
 
